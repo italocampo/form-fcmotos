8e1c92631327728e42f7d470c17a67c7ed460af8
italocampo <italosouzacampos99@gmail.com>
2026-02-01 19:49:20 -0300

    at-19h49

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,8 @@ function Home() {
   return (
     <div className="w-full max-w-5xl flex flex-col items-center animate-fade-in">
       
-      {/* Título Principal */}
-      {/* Título Principal */}
 <h1 className="text-3xl md:text-6xl font-extrabold text-center mb-10 leading-tight">
-  SEJA MEU SÓCIO EM UMA NOVA UNIDADE DA <span className="text-orange-600 whitespace-nowrap">FC MOTOS</span>
+  SEJA MEU SÓCIO EM UMA NOVA UNIDADE DA <span className="text-orange-600 block md:inline">FC MOTOS</span>
 </h1>
 
       {/* Grid de Estatísticas */}
